f0ecd8289e291e0f017e6d1c33a4daac34021868
Wajid Ali <wajidh1997@gmail.com>
2025-05-20 11:13:25 +0300

    use logger middleware

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+// Custom middleware to log request details
+app.use((req, res, next) => {
+    console.log(`Date: ${new Date().toISOString()} - ip: ${req.ip} method: ${req.method} url: '${req.url}'`);
+    next();
+});
+
 // Basic route to handle GET requests
 app.get('/', (req, res) => {
     res.send('Express.js is running perfectly!');
