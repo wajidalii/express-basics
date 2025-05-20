261470d5d838ed96616291d7fd0dbc926c98afc4
Wajid Ali <wajidh1997@gmail.com>
2025-05-20 12:25:36 +0300

    refactor index to user routes from modules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/users');
+const taskRoutes = require('./routes/tasks');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -25,19 +27,9 @@ app.get('/about', (req, res) => {
     res.send('About Page');
 });
 
-app.get('/users/:id', (req, res) => {
-    res.send(`User ID is ${req.params.id}`);
-});
+app.use('/users', userRoutes);
 
-app.get('/search', (req, res) => {
-    const { q } = req.query;
-    res.send(`You searched for: ${q}`);
-});
-
-app.get('/task', (req, res) => res.send('Get task'));
-app.post('/task', (req, res) => res.send('Create task'));
-app.put('/task/:id', (req, res) => res.send(`Update task ${req.params.id}`));
-app.delete('/task/:id', (req, res) => res.send(`Delete task ${req.params.id}`));
+app.use('/tasks', taskRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
