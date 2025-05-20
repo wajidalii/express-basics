bee06044eea5bff29c81961a2d844b37f250f131
Wajid Ali <wajidh1997@gmail.com>
2025-05-20 12:29:48 +0300

    add get task by id route

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Get task'));
+router.get('/:id', (req, res) => res.send(`Get task ${req.params.id}`));
 router.post('/', (req, res) => res.send('Create task'));
 router.put('/:id', (req, res) => res.send(`Update task ${req.params.id}`));
 router.delete('/:id', (req, res) => res.send(`Delete task ${req.params.id}`));
