3c5c5eeff501fb3f9e62da8bb277689429833f63
Wajid Ali <wajidh1997@gmail.com>
2025-05-20 11:47:22 +0300

    create basic multiple routes

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ app.get('/', (req, res) => {
     res.send('Express.js is running perfectly!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/users/:id', (req, res) => {
+    res.send(`User ID is ${req.params.id}`);
+});
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    res.send(`You searched for: ${q}`);
+});
+
+app.get('/task', (req, res) => res.send('Get task'));
+app.post('/task', (req, res) => res.send('Create task'));
+app.put('/task/:id', (req, res) => res.send(`Update task ${req.params.id}`));
+app.delete('/task/:id', (req, res) => res.send(`Delete task ${req.params.id}`));
+
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
