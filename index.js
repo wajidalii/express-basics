562154fc708d900aecff31e3ec55cfeb94cf5716
Wajid Ali <wajidh1997@gmail.com>
2025-05-20 10:25:56 +0300

    create basic server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Basic route to handle GET requests
+app.get('/', (req, res) => {
+    res.send('Express.js is running perfectly!');
+})
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
