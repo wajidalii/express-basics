88fae757e00f0d7d43443ddda4be141cf12b76fd
Wajid Ali <wajidh1997@gmail.com>
2025-05-20 15:12:36 +0300

    refactor rateLimiter to use ip as a map key

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,15 +1,18 @@
 // Rate limiting middleware
 // This middleware limits the number of requests from a single IP
 const rateLimiter = (limit) => {
-    let count = 0;
+    const requestCounts = new Map(); // key: IP or user, value: count
+
     return (req, res, next) => {
-        count++;
-        if (count > limit) {
-            res.send('Too many requests, please try again later.');
-        } else {
-            next();
+        const ip = req.ip;
+        requestCounts.set(ip, (requestCounts.get(ip) || 0) + 1);
+
+        if (requestCounts.get(ip) > limit) {
+            return res.status(429).send('Too many requests from your IP');
         }
+
+        next();
     };
 };
 
-module.exports = rateLimiter;  
+module.exports = rateLimiter;
