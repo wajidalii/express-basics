cfd5c5a0be9b0bf7bed1f36428e7bb3327d55af6
Wajid Ali <wajidh1997@gmail.com>
2025-05-21 16:47:31 +0300

    add user registration and login rules

--- validations/userValidation.js
+++ validations/userValidation.js
@@ -12,6 +12,27 @@ const userFieldsRules = [
         .normalizeEmail(),
 ];
 
+const userRegistrationRules = [
+    ...userFieldsRules,
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isString().withMessage('Password must be a string')
+        .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+        .trim(),
+];
+
+const userLoginRules = [
+    body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email must be valid')
+        .normalizeEmail(),
+
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isString().withMessage('Password must be a string')
+        .trim(),
+];
+
 const userIdParamRules = [
     param('id').isInt().withMessage('ID must be an integer'),
 ];
@@ -20,4 +41,8 @@ exports.createUserRules = [...userFieldsRules];
 
 exports.updateUserRules = [...userIdParamRules, ...userFieldsRules];
 
-exports.userIdParamRules = userIdParamRules;
+exports.userRegistrationRules = [...userRegistrationRules];
+
+exports.userLoginRules = [...userLoginRules];
+
+exports.userIdParamRules = [...userIdParamRules];
