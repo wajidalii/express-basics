c0675418c9d86ea9e4f72f5ae5e2db79cbf9f3e1
Wajid Ali <wajidh1997@gmail.com>
2025-05-21 09:13:47 +0300

    fix password issue

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
