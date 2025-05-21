1b9ef4d884728bb42f5080207a336384ed4cb33b
Wajid Ali <wajidh1997@gmail.com>
2025-05-21 09:29:09 +0300

    add urlencoded

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const rateLimiter = require('./middlewares/rateLimiter');
 const path = require('path');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
