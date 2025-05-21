069c4fe71910a471b2f8badc785b874d68f525bd
Wajid Ali <wajidh1997@gmail.com>
2025-05-21 14:10:05 +0300

    create user validation rules

---
+++ validations/userValidation.js
@@ -0,0 +1,23 @@
+const { body, param } = require('express-validator');
+
+const userFieldsRules = [
+    body('name')
+        .notEmpty().withMessage('Name is required')
+        .isString().withMessage('Name must be a string')
+        .trim(),
+
+    body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email must be valid')
+        .normalizeEmail(),
+];
+
+const userIdParamRules = [
+    param('id').isInt().withMessage('ID must be an integer'),
+];
+
+exports.createUserRules = [...userFieldsRules];
+
+exports.updateUserRules = [...userIdParamRules, ...userFieldsRules];
+
+exports.userIdParamRules = userIdParamRules;
