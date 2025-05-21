24e9786b794f7d9ee9c27c5d39ad49b2591fead6
Wajid Ali <wajidh1997@gmail.com>
2025-05-21 09:42:54 +0300

    modify userController to use userService

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,46 @@
-export function getAllUsers(req, res) {
-    res.send('Get all users');
-}
+const userService = require('../services/userService');
 
-export function getUserById(req, res) {
-    res.send(`Get user with ID ${req.params.id}`);
-}
+exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await userService.getAllUsers();
+        res.json(users);
+    } catch (err) {
+        next(err);
+    }
+};
 
-export function createUser(req, res) {
-    const { name, email } = req.body;
-    res.status(201).json({ message: 'User created', name, email });
-}
+exports.getUserById = async (req, res, next) => {
+    try {
+        const user = await userService.getUserById(req.params.id);
+        res.json(user);
+    } catch (err) {
+        next(err);
+    }
+};
 
-export function updateUser(req, res) {
-    res.send(`Update user with ID ${req.params.id}`);
-}
+exports.createUser = async (req, res, next) => {
+    try {
+        const user = await userService.createUser(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        next(err);
+    }
+};
 
-export function deleteUser(req, res) {
-    res.send(`Delete user with ID ${req.params.id}`);
-}  
+exports.updateUser = async (req, res, next) => {
+    try {
+        const user = await userService.updateUser(req.params.id, req.body);
+        res.json(user);
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.deleteUser = async (req, res, next) => {
+    try {
+        await userService.deleteUser(req.params.id);
+        res.status(204).send();
+    } catch (err) {
+        next(err);
+    }
+};
