59af31047e3d9a574e89223877a6130c0f88a527
Wajid Ali <wajidh1997@gmail.com>
2025-05-21 11:15:26 +0300

    add validation in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,13 @@
 const userService = require('../services/userService');
+const { validationResult } = require('express-validator');
+
+const handleValidation = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
 
 exports.getAllUsers = async (req, res, next) => {
     try {
@@ -18,31 +27,40 @@ exports.getUserById = async (req, res, next) => {
     }
 };
 
-exports.createUser = async (req, res, next) => {
-    try {
-        const user = await userService.createUser(req.body);
-        res.status(201).json(user);
-    } catch (err) {
-        next(err);
+exports.createUser = [
+    handleValidation,
+    async (req, res, next) => {
+        try {
+            const user = await userService.createUser(req.body);
+            res.status(201).json(user);
+        } catch (err) {
+            next(err);
+        }
     }
-};
+];
 
-exports.updateUser = async (req, res, next) => {
-    try {
-        const affected = await userService.updateUser(req.params.id, req.body);
-        if (!affected) return res.status(404).send('User not found');
-        res.send('User updated');
-    } catch (err) {
-        next(err);
+exports.updateUser = [
+    handleValidation,
+    async (req, res, next) => {
+        try {
+            const affected = await userService.updateUser(req.params.id, req.body);
+            if (!affected) return res.status(404).send('User not found');
+            res.send('User updated');
+        } catch (err) {
+            next(err);
+        }
     }
-};
+];
 
-exports.deleteUser = async (req, res, next) => {
-    try {
-        const affected = await userService.deleteUser(req.params.id);
-        if (!affected) return res.status(404).send('User not found');
-        res.send('User deleted');
-    } catch (err) {
-        next(err);
+exports.deleteUser = [
+    handleValidation,
+    async (req, res, next) => {
+        try {
+            const affected = await userService.deleteUser(req.params.id);
+            if (!affected) return res.status(404).send('User not found');
+            res.send('User deleted');
+        } catch (err) {
+            next(err);
+        }
     }
-};
+];
