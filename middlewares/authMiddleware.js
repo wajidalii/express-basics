e3250580d66d367f9d94fc0eebc1a62153748f32
Wajid Ali <wajidh1997@gmail.com>
2025-05-21 17:10:24 +0300

    fix crash issue

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken');
 const protect = (req, res, next) => {
     const authHeader = req.headers['authorization'];
 
-    if (!authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Token missing' });
-    }
-
-    const token = authHeader.split(' ')[1];
-
     try {
+
+        if (!authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({ message: 'Token missing' });
+        }
+
+        const token = authHeader.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
