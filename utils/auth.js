feda48f033142df48b711a046625389f034a774b
Wajid Ali <wajidh1997@gmail.com>
2025-05-22 14:39:37 +0300

    create auth utility file

---
+++ utils/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const { parseDuration } = require('./time');
+
+exports.signAccessToken = (user) =>
+    jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+
+exports.signRefreshToken = (user) => {
+    const token = jwt.sign({ id: user.id }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+    });
+    const expires = new Date(Date.now() +
+        parseDuration(process.env.REFRESH_TOKEN_EXPIRES_IN)
+    );
+    return { token, expires };
+};
