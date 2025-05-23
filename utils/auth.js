763204c08bb7169be546b7d83650dccf9c963e40
Wajid Ali <wajidh1997@gmail.com>
2025-05-23 15:15:06 +0300

    add userRole when signing accessToken

--- utils/auth.js
+++ utils/auth.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 const { parseDuration } = require('./time');
 
 exports.signAccessToken = (user) =>
-    jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+    jwt.sign({ id: user.id, email: user.email, role: user.role }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
 
 exports.signRefreshToken = (user) => {
-    const token = jwt.sign({ id: user.id }, process.env.REFRESH_TOKEN_SECRET, {
+    const token = jwt.sign({ id: user.id, role: user.role }, process.env.REFRESH_TOKEN_SECRET, {
         expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
     });
     const expires = new Date(Date.now() +
