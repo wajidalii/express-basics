4d6737c7843bdf5128a7a788d8938fc8ad4e5938
Wajid Ali <wajidh1997@gmail.com>
2025-05-24 17:58:02 +0300

    modify orderRepo

--- repositories/orderRepository.js
+++ repositories/orderRepository.js
@@ -1,7 +1,18 @@
 class OrderRepository {
-    async findById(connection, id) {
+    async findOrderById(connection, id) {
         const [rows] = await connection.query('SELECT * FROM orders WHERE id = ?', [id]);
         return rows[0];
+    }
+
+    async findOrderItemsByOrderId(connection, orderId) {
+        const [rows] = await connection.query(`
+            SELECT * FROM order_items WHERE orderId = ?`, [orderId]);
+        return rows;
+    }
+
+    async findOrdersByUserId(connection, userId) {
+        const [rows] = await connection.query('SELECT * FROM orders WHERE userId = ?', [userId]);
+        return rows;
     }
 
     async createOrder(connection, userId) {
@@ -39,8 +50,13 @@ class OrderRepository {
         return result.affectedRows;
     }
 
-    async delete(connection, id) {
+    async deleteOrder(connection, id) {
         const [result] = await connection.query('DELETE FROM orders WHERE id = ?', [id]);
+        return result.affectedRows;
+    }
+
+    async deleteItemByOrderId(connection, orderId) {
+        const [result] = await connection.query('DELETE FROM order_items WHERE orderId = ?', [orderId]);
         return result.affectedRows;
     }
 }
