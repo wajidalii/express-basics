0daaf9a04c0329e62c2896db2121837c05e2a3a7
Wajid Ali <wajidh1997@gmail.com>
2025-05-24 11:10:03 +0300

    add file size limit and file types

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -11,6 +11,21 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage });
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5 // 5 MB
+    },
+    fileFilter: (req, file, cb) => {
+        const filetypes = /jpeg|jpg|png|gif|pdf/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+        cb(new Error('Error: File upload only supports the following filetypes - ' + filetypes));
+    }
+});
 
 module.exports = upload;
