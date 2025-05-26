6fb3ac74a819c53bbaa4f74cbfda1b3ef72ee59a
Wajid Ali <wajidh1997@gmail.com>
2025-05-26 16:55:17 +0300

    comment out clustering

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const cluster = require('cluster');
-const os = require('os');
+// const cluster = require('cluster');
+// const os = require('os');
 
-if (cluster.isMaster) {
-    const cpuCount = os.cpus().length;
-    console.log(`Master process is running. Forking ${cpuCount} workers...`);
+// if (cluster.isMaster) {
+//     const cpuCount = os.cpus().length;
+//     console.log(`Master process is running. Forking ${cpuCount} workers...`);
 
-    for (let i = 0; i < cpuCount; i++) {
-        cluster.fork();
-    }
+//     for (let i = 0; i < cpuCount; i++) {
+//         cluster.fork();
+//     }
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.warn(`Worker ${worker.process.pid} died. Spawning a new one.`);
-        cluster.fork();
-    });
-} else {
-    // Each worker loads the same Express app
-    const app = require('./index');
-    const config = require('./config');
-    app.listen(config.port, () =>
-        console.log(`Worker ${process.pid} listening on port ${config.port}`)
-    );
-}
+//     cluster.on('exit', (worker, code, signal) => {
+//         console.warn(`Worker ${worker.process.pid} died. Spawning a new one.`);
+//         cluster.fork();
+//     });
+// } else {
+//     // Each worker loads the same Express app
+//     const app = require('./index');
+//     const config = require('./config');
+//     app.listen(config.port, () =>
+//         console.log(`Worker ${process.pid} listening on port ${config.port}`)
+//     );
+// }
+
+const app = require('./index');
+const config = require('./config');
+app.listen(config.port, () =>
+    console.log(`Server listening on port ${config.port}`)
+);
