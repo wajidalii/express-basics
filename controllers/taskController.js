e3d8bd6e1782ef649d66a97df946792e8384b8b9
Wajid Ali <wajidh1997@gmail.com>
2025-05-26 13:16:20 +0300

    fix issue

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,20 +1,20 @@
-export function getAllTasks(req, res) {
+exports.getAllTasks = async (req, res) => {
     res.send('Get all tasks');
 }
 
-export function getTaskById(req, res) {
+exports.getTaskById = async (req, res) => {
     res.send(`Get task with ID ${req.params.id}`);
 }
 
-export function createTask(req, res) {
+exports.createTask = async (req, res) => {
     const { title, description } = req.body;
     res.status(201).json({ message: 'Task created', title, description });
 }
 
-export function updateTask(req, res) {
+exports.updateTask = async (req, res) => {
     res.send(`Update task with ID ${req.params.id}`);
 }
 
-export function deleteTask(req, res) {
+exports.deleteTask = async (req, res) => {
     res.send(`Delete task with ID ${req.params.id}`);
 }
