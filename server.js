ccc25125d4add40f4652da73bf062190120d2aaa
Wajid Ali <wajidh1997@gmail.com>
2025-05-27 16:28:35 +0300

    create sockets server

--- server.js
+++ server.js
@@ -22,8 +22,41 @@
 //     );
 // }
 
+const http = require('http');
 const app = require('./index');
 const config = require('./config');
-app.listen(config.port, () =>
-    console.log(`Server listening on port ${config.port}`)
+
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: config.frontendUrl,
+        methods: ['GET', 'POST'],
+    }
+});
+
+module.exports = io;
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+        console.log(`User ${socket.id} joined room: ${room}`);
+    })
+
+    socket.on('chatMessage', ({ room, msg, user }) => {
+        console.log(`Chat message from ${user} in room ${room}: ${msg}`);
+        io.to(room).emit('chatMessage', { user, msg, time: new Date() });
+    });
+
+    socket.on('userTyping', ({ room, user }) => {
+        console.log(`User ${user} is typing in ${room}`);
+        io.to(room).emit('userTyping', { user });
+    });
+    socket.on('disconnect', (reason) => {
+        console.log(`User disconnected: ${socket.id}, reason: ${reason}`);
+    })
+});
+
+server.listen(config.port, () =>
+    console.log(`Server & WebSocket listening on port ${config.port}`)
 );
